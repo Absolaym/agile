c136207d258edab110d45ac6f584fe545868a055
Marie France Gonzalez <mariefrgon@gmail.com>
2018-10-21 13:09:50 +0200

    added model objects for UC1 and sample xml files

---
+++ agile/src/main/java/model/Section.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Section {
+	
+	private String streetName;
+	private double length;
+	private Intersection startIntersection;
+	private Intersection endIntersection;
+	
+	public Section(String aStreetName, double aLength, Intersection aStartIntersection, Intersection anEndIntersection) {
+		setStreetName(aStreetName);
+		setLength(aLength);
+		setStartIntersection(aStartIntersection);
+		setEndIntersection(anEndIntersection);
+	}
+	
+	
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public Intersection getStartIntersection() {
+		return startIntersection;
+	}
+	public void setStartIntersection(Intersection startIntersection) {
+		this.startIntersection = startIntersection;
+	}
+	public Intersection getEndIntersection() {
+		return endIntersection;
+	}
+	public void setEndIntersection(Intersection endIntersection) {
+		this.endIntersection = endIntersection;
+	}
+	
+
+}
