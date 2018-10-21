c5a438a5af5f9d3f935482fec68fae7eb3991c02
oliviacaraiman <caraimanolivia@gmail.com>
2018-10-21 11:58:56 +0200

    modifications on delivery requests

--- agile/src/main/java/pld/agile/view/DeliveryRequestView.java
+++ agile/src/main/java/pld/agile/view/DeliveryRequestView.java
@@ -11,25 +11,43 @@ import javax.swing.*;
 
 public class DeliveryRequestView extends JPanel {
 
-    private JTable deliveryRequests;
+    private final JTable deliveryRequests;
     private JButton loadDeliveryRequestsButton;
+    private final int loadDeliveryRequestsButtonWidth = 200;
+    private final int loadDeliveryRequestsButtonHeight = 30;
+    private final int buttonLocationY = 50;
     private int height = 800;
     private int width = 300;
 
     public DeliveryRequestView(Window w, Controller c) {
         super();
         setLayout(null);
+        
+        //set button
         setBorder(BorderFactory.createTitledBorder("Delivery requests :"));
         loadDeliveryRequestsButton = new JButton("Load delivery requests");
         loadDeliveryRequestsButton.addActionListener(new ButtonListener(c));
-        loadDeliveryRequestsButton.setSize(200,30);
-        loadDeliveryRequestsButton.setLocation(0, 50);
-        
+        loadDeliveryRequestsButton.setSize(loadDeliveryRequestsButtonWidth,loadDeliveryRequestsButtonHeight);
+        loadDeliveryRequestsButton.setLocation(0, buttonLocationY);
         add(loadDeliveryRequestsButton);
+        
+        //set table
+        String column[] = {"Address", "Delivery time", "Circuit"};
+        final String data[][]={ {"101","12:00","1"},    
+                          {"102","14:50","2"},    
+                          {"101","15:02","7"}};
+        deliveryRequests = new JTable(data,column) {
+             public boolean isCellEditable(int row,int column){
+                return false;}
+        };
+        JScrollPane scrollPanel = new JScrollPane(deliveryRequests); 
+        scrollPanel.setBounds(30,40,width - 10, height - 10);
+        scrollPanel.setLocation(0, 50 + loadDeliveryRequestsButtonHeight + 50);
+        add(scrollPanel);
         
         setBackground(Color.LIGHT_GRAY);
         w.getContentPane().add(this);
-       // w.getContentPane().add(loadDeliveryRequestsButton);
+
     }
 
     public void paintComponent(Graphics g) {
