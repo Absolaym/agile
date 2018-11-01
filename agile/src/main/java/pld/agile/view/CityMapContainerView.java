58ec884484ab5d9e485bebb47a5631372493cb91
Johnny Guye <johnnyq@hotmail.fr>
2018-11-01 15:24:05 +0100

    reduce the alogithmic complexicity of the map painting

--- agile/src/main/java/pld/agile/view/CityMapContainerView.java
+++ agile/src/main/java/pld/agile/view/CityMapContainerView.java
@@ -2,7 +2,11 @@ package pld.agile.view;
 
 import controller.Controller;
 
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
@@ -35,13 +39,11 @@ public class CityMapContainerView extends JPanel implements Observer {
 	// Map elements
 	private final int DELIV_DOT_SIZE = 15;
 	private Delivery selectedDelivery = null;
-	private ArrayList<Ellipse2D> deliveriesPoints;
 
 	public CityMapContainerView(Window w, Controller c) {
 		super();
 		this.controller = c;
 		this.window = w;
-		deliveriesPoints = new ArrayList<>();
 
 		setLayout(null);
 		setBorder(BorderFactory.createTitledBorder("City Map :"));
@@ -202,7 +204,6 @@ public class CityMapContainerView extends JPanel implements Observer {
 	private Geolocation getOrigin(CityMap cityMap) {
 		Geolocation origin = null;
 
-		// Origin to the top left corner
 		for (Intersection inter : cityMap.getIntersections().values()) {
 			if (origin == null) {
 				origin = inter.getGeolocation().copy();
@@ -214,27 +215,67 @@ public class CityMapContainerView extends JPanel implements Observer {
 		return origin;
 	}
 
+	private void colorDelivery(Graphics g, Color c, Geolocation geolocation, Geolocation origin, int dotSize ) {
+		
+			if (geolocation != null) {
+				Geolocation geo = geolocationToPixels(origin, geolocation);
+				//when user clicks on a particular delivery
+				if (selectedDelivery != null && selectedDelivery.getGeolocation() == geolocation) {
+					g.setColor(Color.YELLOW);
+					Geolocation selectedGeo = geolocationToPixels(origin, selectedDelivery.getGeolocation());
+					g.fillArc((int) selectedGeo.getLongitude() - dotSize / 2, (int) selectedGeo.getLatitude() - dotSize / 2, dotSize, dotSize, 0, 360);;
+					// should call a method in delivery request to notify that a row in a table has to be colored in yellow
+					// the following method doesn't work
+					window.getDeliveryRequestPanel().colorTable(1,Color.YELLOW);
+				}
+				else {
+					g.setColor(c);
+					g.fillArc((int) geo.getLongitude() - dotSize / 2, (int) geo.getLatitude() - dotSize / 2, dotSize, dotSize, 0, 360);
+				}
+			}
+
+		}
+
+		private void colorSections(Graphics g, Color c, java.util.List<Section> sections, Geolocation origin) {
+			g.setColor(c);
+			int lineThickness = 4;
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+
+			for (Section sec : sections) {
+				Geolocation start = sec.getStartIntersection().getGeolocation();
+				Geolocation end = sec.getEndIntersection().getGeolocation();
+
+				Geolocation pxStart = this.geolocationToPixels(origin, start);
+				Geolocation pxEnd = this.geolocationToPixels(origin, end);
+
+				if(sec == this.hoveredSection) {
+					g2.setStroke(new BasicStroke(lineThickness * 2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+					g.drawLine((int) pxStart.getLongitude(), (int) pxStart.getLatitude(), (int) pxEnd.getLongitude(), (int) pxEnd.getLatitude());
+					g2.setStroke(new BasicStroke(lineThickness));
+				} else {
+					g.drawLine((int) pxStart.getLongitude(), (int) pxStart.getLatitude(), (int) pxEnd.getLongitude(), (int) pxEnd.getLatitude());
+				}
+			}
+		}
+	
 	private void drawDeliveriesOnCityMap(Graphics g) {
 		CityMap cityMap = controller.getModel().getCityMap();
-		if (cityMap == null) {
-			return;
-		}
+		if (cityMap == null) return;
 
 		DeliveryRequest dr = controller.getModel().getDeliveryRequest();
-		if (dr == null) {
-			return;
-		}
+		if (dr == null) return;
+		
+		Geolocation origin = getOrigin( cityMap );
 
 		LinkedList<Delivery> delivs = dr.getDeliveries();
-		if (delivs.size() > 0) {
-			for (Delivery d : delivs) {
-				colorDelivery(g, Color.green, d.getGeolocation(), cityMap, DELIV_DOT_SIZE, deliveriesPoints);
-			}
+		for (Delivery d : delivs) {
+			colorDelivery( g, Color.green, d.getGeolocation(), origin, DELIV_DOT_SIZE );
 		}
 
 		Intersection warehouseIntersection = cityMap.getIntersectionById(dr.getWarehouseAddress());
 		if (warehouseIntersection != null) {
-			colorDelivery(g, Color.red, warehouseIntersection.getGeolocation(), cityMap, DELIV_DOT_SIZE, deliveriesPoints);
+			colorDelivery( g, Color.red, warehouseIntersection.getGeolocation(), origin, DELIV_DOT_SIZE );
 		}
 	}
 	
@@ -252,62 +293,13 @@ public class CityMapContainerView extends JPanel implements Observer {
 			int i = 0;
 			Color c = new Color(180, Math.floorMod(50 + 40 * i, 100), Math.floorMod(120 + 40 * i, 100));
 			for (Trip trip : circuit.getTrips()) {
-				colorSections(g, c, trip.getSections(), cityMap);
+				colorSections(g, c, trip.getSections(), origin);
 			}
 
 			for (Delivery deliv : circuit.getDeliveries()) {
-				colorDelivery(g, c, deliv.getGeolocation(), cityMap, DELIV_DOT_SIZE, deliveriesPoints);
+				colorDelivery( g, c, deliv.getGeolocation(), origin, DELIV_DOT_SIZE );
 			}
 			i++;
-		}
-	}
-
-	private void colorDelivery(Graphics g, Color c, Geolocation geolocation, CityMap cm, int dotSize, ArrayList<Ellipse2D> delivPoints) {
-
-		Geolocation origin = getOrigin(cm);
-		if (geolocation != null) {
-			Geolocation geo = geolocationToPixels(origin, geolocation);
-			//when user clicks on a particular delivery
-			if (selectedDelivery != null && selectedDelivery.getGeolocation() == geolocation) {
-				g.setColor(Color.YELLOW);
-				Geolocation selectedGeo = geolocationToPixels(origin, selectedDelivery.getGeolocation());
-				g.fillArc((int) selectedGeo.getLongitude() - dotSize / 2, (int) selectedGeo.getLatitude() - dotSize / 2, dotSize, dotSize, 0, 360);;
-				// should call a method in delivery request to notify that a row in a table has to be colored in yellow
-				// the following method doesn't work
-				window.getDeliveryRequestPanel().colorTable(1,Color.YELLOW);
-			}
-			else {
-				g.setColor(c);
-				g.fillArc((int) geo.getLongitude() - dotSize / 2, (int) geo.getLatitude() - dotSize / 2, dotSize, dotSize, 0, 360);
-			}
-		}
-
-	}
-
-	private void colorSections(Graphics g, Color c, java.util.List<Section> sections, CityMap cm) {
-		g.setColor(c);
-		int lineThickness = 4;
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-
-		Geolocation origin = getOrigin(cm);
-
-		for (Section sec : sections) {
-			Geolocation start = sec.getStartIntersection().getGeolocation();
-			Geolocation end = sec.getEndIntersection().getGeolocation();
-
-			Geolocation pxStart = this.geolocationToPixels(origin, start);
-			Geolocation pxEnd = this.geolocationToPixels(origin, end);
-
-			if(sec == this.hoveredSection) {
-				g2.setStroke(new BasicStroke(lineThickness * 2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-				g.drawLine((int) pxStart.getLongitude(), (int) pxStart.getLatitude(), (int) pxEnd.getLongitude(), (int) pxEnd.getLatitude());
-				g2.setStroke(new BasicStroke(lineThickness));
-			} else {
-				g.drawLine((int) pxStart.getLongitude(), (int) pxStart.getLatitude(), (int) pxEnd.getLongitude(), (int) pxEnd.getLatitude());
-			}
-
-			
 		}
 	}
 
@@ -321,7 +313,7 @@ public class CityMapContainerView extends JPanel implements Observer {
 		}
 		Geolocation origin = getOrigin(cityMap);
 
-		colorSections(g, new Color(100, 100, 105), cityMap.getSections(), cityMap);
+		colorSections(g, new Color(100, 100, 105), cityMap.getSections(), origin);
 		g.setColor(new Color(180, 140, 180));
 
 		for (Intersection inter : cityMap.getIntersections().values()) {
