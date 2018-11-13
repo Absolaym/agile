4b7461a6720c04bcb9b3e0b0db5e1211d4602acf
PAGILLES <pagilles@hotmail.com>
2018-11-14 00:05:00 +0100

    updated Model class

--- agile/src/main/java/model/Model.java
+++ agile/src/main/java/model/Model.java
@@ -19,158 +19,157 @@ import utils.ShortestPathComputer;
  */
 public class Model {
 
-	private CityMap cityMap;
-	private DeliveryRequest deliveryRequest;
-	private HashMap<String,HashMap<String,Trip>> shortestPaths;
-	private LinkedList<Circuit> circuits;
-	private int numberOfCouriers;
+    private CityMap cityMap;
+    private DeliveryRequest deliveryRequest;
+    private HashMap<String,HashMap<String,Trip>> shortestPaths;
+    private LinkedList<Circuit> circuits;
+    private int numberOfCouriers;
 
-	private static Model INSTANCE = null;
+    private static Model INSTANCE = null;
 
-	private Model(){
-		//        deliveryRequest = new DeliveryRequest();
-		//        circuits = new LinkedList<Circuit>();
-		cityMap = new CityMap();
-		deliveryRequest = null;
-		circuits = null;
-		numberOfCouriers = -1;
-	}
+    private Model(){
+//        deliveryRequest = new DeliveryRequest();
+//        circuits = new LinkedList<Circuit>();
+        cityMap = new CityMap();
+        deliveryRequest = null;
+        circuits = null;
+        numberOfCouriers = -1;
+    }
 
-	public static Model getInstance(){
-		if(INSTANCE == null){
-			INSTANCE = new Model();
-		}
-		return INSTANCE;
-	}
+    public static Model getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new Model();
+        }
+        return INSTANCE;
+    }
 
-	public LinkedList<Circuit> getCircuits() {
-		return circuits;
-	}
+    public LinkedList<Circuit> getCircuits() {
+        return circuits;
+    }
 
-	public void setCircuits(LinkedList<Circuit> circuits) {
-		this.circuits = circuits;
-	}
-	public CityMap getCityMap() {
-		return cityMap;
-	}
+    public void setCircuits(LinkedList<Circuit> circuits) {
+        this.circuits = circuits;
+    }
+    public CityMap getCityMap() {
+        return cityMap;
+    }
 
-	public void setCityMap(CityMap cityMap) {
-		this.cityMap = cityMap;
-	}
+    public void setCityMap(CityMap cityMap) {
+        this.cityMap = cityMap;
+    }
 
-	public DeliveryRequest getDeliveryRequest() {
-		return deliveryRequest;
-	}
+    public DeliveryRequest getDeliveryRequest() {
+        return deliveryRequest;
+    }
 
-	public void setDeliveryRequest(DeliveryRequest deliveryRequest) {
-		this.deliveryRequest = deliveryRequest;
-	}
+    public void setDeliveryRequest(DeliveryRequest deliveryRequest) {
+        this.deliveryRequest = deliveryRequest;
+    }
 
-	public void computeCircuits() {
+    public void computeCircuits() {
 
-		if(this.deliveryRequest == null) {
-			ErrorLogger.getInstance().log( ProjectError.NO_DR_BEF_CIRCUIT );
-			return;
-		}
+        if(this.deliveryRequest == null) {
+            ErrorLogger.getInstance().log( ProjectError.NO_DR_BEF_CIRCUIT );
+            return;
+        }
 
-		if(this.numberOfCouriers <= 0) {
-			ErrorLogger.getInstance().log( ProjectError.NO_COURRIER_ASSIGNED );
-			return;
-		}
+        if(this.numberOfCouriers <= 0) {
+            ErrorLogger.getInstance().log( ProjectError.NO_COURRIER_ASSIGNED );
+            return;
+        }
 
-		if(this.numberOfCouriers > this.deliveryRequest.getDeliveries().size()) {
-			ErrorLogger.getInstance().log( ProjectError.TOO_MUCH_COURRIER );
-			return;
-		}
+        if(this.numberOfCouriers > this.deliveryRequest.getDeliveries().size()) {
+            ErrorLogger.getInstance().log( ProjectError.TOO_MUCH_COURRIER );
+            return;
+        }
 
-		if(this.shortestPaths == null) {
-			ErrorLogger.getInstance().log( ProjectError.TIMEOUT );
-			return;
-		}
+        if(this.shortestPaths == null) {
+            ErrorLogger.getInstance().log( ProjectError.TIMEOUT );
+            return;
+        }
 
-		CircuitComputer circuitComputer = new CircuitComputer();
+        CircuitComputer circuitComputer = new CircuitComputer();
 
-		circuitComputer.init(this.deliveryRequest, this.shortestPaths);
-		circuitComputer.execute(this.numberOfCouriers);
-		this.setCircuits(circuitComputer.result());
+        circuitComputer.init(this.deliveryRequest, this.shortestPaths);
+        circuitComputer.execute(this.numberOfCouriers);
+        this.setCircuits(circuitComputer.result());
 
-		deliveryRequest = new DeliveryRequest();
-		for(Circuit circuit : circuits){
-				for(Delivery delivery : circuit.getDeliveries()){
-						deliveryRequest.addDelivery(delivery);
-				}
-		}
+        deliveryRequest = new DeliveryRequest();
+        for(Circuit circuit : circuits){
+            for(Delivery delivery : circuit.getDeliveries()){
+                deliveryRequest.addDelivery(delivery);
+            }
+        }
 
-	}
+    }
 
-	public int getNumberOfCouriers() {
-		return numberOfCouriers;
-	}
+    public int getNumberOfCouriers() {
+        return numberOfCouriers;
+    }
 
-	public void setNumberOfCouriers(int numberOfCouriers) {
-		if (this.deliveryRequest == null) {
-				System.out.println("Error: Delivery request needed to set number of couriers");
-				return;
-		}
-		if (this.deliveryRequest.getDeliveries().size() < numberOfCouriers) {
-				System.out.println("Warning: number of courriers exceeds number of deliveries");//error
-				this.numberOfCouriers = this.deliveryRequest.getDeliveries().size();
-				return;
-		}
-		this.numberOfCouriers = numberOfCouriers;
-	}
+    public void setNumberOfCouriers(int numberOfCouriers) {
+        if (this.deliveryRequest == null) {
+            System.out.println("Error: Delivery request needed to set number of couriers");
+            return;
+        }
+        if (this.deliveryRequest.getDeliveries().size() < numberOfCouriers) {
+            System.out.println("Warning: number of courriers exceeds number of deliveries");//error
+            this.numberOfCouriers = this.deliveryRequest.getDeliveries().size();
+            return;
+        }
+        this.numberOfCouriers = numberOfCouriers;
+    }
 
-	public void setSelectedDelivery(Delivery delivery){
-		LinkedList<Delivery> deliveries = deliveryRequest.getDeliveries();
+    public void setSelectedDelivery(Delivery delivery){
+        LinkedList<Delivery> deliveries = deliveryRequest.getDeliveries();
 
-		for(int i =0; i < deliveries.size(); i++){
-			if(deliveries.get(i) == delivery)
-				deliveries.get(i).setIsSelected(true);
-			else
-				deliveries.get(i).setIsSelected(false);
-		}
+        for(int i =0; i < deliveries.size(); i++){
+            if(deliveries.get(i) == delivery)
+                deliveries.get(i).setIsSelected(true);
+            else
+                deliveries.get(i).setIsSelected(false);
+        }
+    }
 
-	}
+    public void computeShortestPaths(){
+        if(this.cityMap == null) {
+            System.out.println("Error: cannot compute shortest path without a city map");//error
+            return;
+        }
+        if(this.deliveryRequest == null) {
+            System.out.println("Error: cannot compute shortest path without a delivery request");//error
+            return;
+        }
+        ShortestPathComputer shortestPathComputer = new ShortestPathComputer();
+        shortestPathComputer.init(cityMap, deliveryRequest);
+        shortestPathComputer.computeAllShortestPaths();
+        this.shortestPaths = shortestPathComputer.result();
 
-	public void computeShortestPaths(){
-		if(this.cityMap == null) {
-			System.out.println("Error: cannot compute shortest path without a city map");//error
-			return;
-		}
-		if(this.deliveryRequest == null) {
-			System.out.println("Error: cannot compute shortest path without a delivery request");//error
-			return;
-		}
-		ShortestPathComputer shortestPathComputer = new ShortestPathComputer();
-		shortestPathComputer.init(cityMap, deliveryRequest);
-		shortestPathComputer.computeAllShortestPaths();
-		this.shortestPaths = shortestPathComputer.result();
-
-	}
+    }
 
 
-	public void resetShortestPaths(){
-		this.shortestPaths = null;
-	}
+    public void resetShortestPaths(){
+        this.shortestPaths = null;
+    }
 
-	public void addDelivery(Delivery delivery) {
-		deliveryRequest.addDelivery(delivery);
-		ShortestPathComputer shortestPathComputer = new ShortestPathComputer();
-		shortestPathComputer.init(cityMap, deliveryRequest);
-		shortestPathComputer.setShortestPathsForNewDelivery(delivery, this.shortestPaths);
-		this.shortestPaths = shortestPathComputer.result();
-	}
+    public void addDelivery(Delivery delivery) {
+        deliveryRequest.addDelivery(delivery);
+        ShortestPathComputer shortestPathComputer = new ShortestPathComputer();
+        shortestPathComputer.init(cityMap, deliveryRequest);
+        shortestPathComputer.setShortestPathsForNewDelivery(delivery, this.shortestPaths);
+        this.shortestPaths = shortestPathComputer.result();
+    }
 
-	public Trip getTripBetweenIntersections(String originAddress, String targetAddress) {
-		if(this.shortestPaths == null) {
-			System.out.println("Error: shortest paths have not yet been computed");//error
-			return null;
-		}
-		HashMap<String, Trip> shortestPathsFromOrigin = this.shortestPaths.get(originAddress);
-		if(shortestPathsFromOrigin == null) {
-			System.out.println("Error: shortest paths from delivery " + originAddress +" were not found");//error
-			return null;
-		}
-		return shortestPathsFromOrigin.get(targetAddress);
-	}
+    public Trip getTripBetweenIntersections(String originAddress, String targetAddress) {
+        if(this.shortestPaths == null) {
+            System.out.println("Error: shortest paths have not yet been computed");//error
+            return null;
+        }
+        HashMap<String, Trip> shortestPathsFromOrigin = this.shortestPaths.get(originAddress);
+        if(shortestPathsFromOrigin == null) {
+            System.out.println("Error: shortest paths from delivery " + originAddress +" were not found");//error
+            return null;
+        }
+        return shortestPathsFromOrigin.get(targetAddress);
+    }
 }
